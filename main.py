50c7e3a0d7ffc1455dd0f3b4c20ac1302d55dca2
Leandro Guido Lorenzini <leandroguidos@gmail.com>
2025-06-16 20:44:55 -0300

    primeira versao

--- main.py
+++ main.py
@@ -7,32 +7,34 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, TensorDataset
 from torch.utils.data.distributed import DistributedSampler
 from utils import ComplexNeuralNetwork, generate_dummy_data, evaluate_model
+import datetime
 
 def cleanup():
     dist.destroy_process_group()
 
-def run_ddp_training(rank, world_size, master_addr, master_port, num_epochs=10):
+def run_ddp_training(rank, world_size, master_addr, master_port, num_epochs=10, timeout = 1):
     print(f"Process {rank}: Inicializando...")
     
     dist.init_process_group(
         backend="gloo",
         init_method=f"tcp://{master_addr}:{master_port}",
         rank=rank,
-        world_size=world_size
+        world_size=world_size,
+        timeout=datetime.timedelta(seconds=timeout)
     )
 
     print(f"Process {rank}: Process group inicializado.")
 
-
+    # criacao do dataset com dados sinteticos
     X_train, y_train, X_test, y_test = generate_dummy_data() 
     input_dim = X_train.shape[1]
     output_dim = 1
-
     train_dataset = TensorDataset(X_train, y_train)
-    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
+
+    # divide os dados entre os nós
+    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank) 
 
     train_dataloader = DataLoader(train_dataset, batch_size=64, sampler=train_sampler) 
-
 
     model = ComplexNeuralNetwork(input_dim, hidden_dim1=128, hidden_dim2=64, output_dim=output_dim)
     model = DDP(model)
@@ -51,8 +53,7 @@ def run_ddp_training(rank, world_size, master_addr, master_port, num_epochs=10):
             loss.backward()
             optimizer.step()
 
-            if i % 10 == 0:
-                print(f"\nProcess {rank}: Epoch {epoch+1}, Batch {i+1}/{len(train_dataloader)}, Loss: {loss.item():.4f}")
+            print(f"\nProcess {rank}: Epoch {epoch+1}, Batch {i+1}/{len(train_dataloader)}, Loss: {loss.item():.4f}")
 
         if rank == 0:
             dist.barrier() 
